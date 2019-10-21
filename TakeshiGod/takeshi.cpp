b8258ede3f7caa25da804d34477d7f0893e00020
danielc6754 <daniel.c6754@gmail.com>
2019-10-21 15:20:54 -0700

    working main file

--- TakeshiGod/takeshi.cpp
+++ TakeshiGod/takeshi.cpp
@@ -1,6 +1,34 @@
+#define OLC_PGE_APPLICATION
 
+#include "PixelGameEngine.h"
 
-int main()
-{
-    return 0;
+class BeatUp : public olc::PixelGameEngine {
+public:
+	BeatUp() {
+		sAppName = "Takeshi Beat Up";
+	}
+
+private:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// called once per frame, draws random coloured pixels
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+		return true;
+	}
+};
+
+int main() {
+	BeatUp game;
+
+	if (game.Construct(1200, 900, 1, 1))
+		game.Start();
+
+	return 0;
 }
